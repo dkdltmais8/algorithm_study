18cf0e5560b20d629a2c2db27ce0a947f0a7561f
이종은 <dkdltmais9@gmail.com>
2021-09-17 21:18:15 +0900

    프로그래머스 - 124 나라의 숫자

--- 프로그래머스/Lv2/124 나라의 숫자.py
+++ 프로그래머스/Lv2/124 나라의 숫자.py
@@ -1,3 +1,25 @@
 def solution(n):
-    answer = ''
+    def cal(n):
+        answer = ''
+        while n > 0:
+            if str(n%3) == '0':
+                answer += '4'
+                n = n//3-1
+            else:
+                answer += str(n%3)
+                n //= 3
+        return answer
+    answer = cal(n)
+    answer = answer[::-1]
+    print(n,answer)
     return answer
+solution(1)
+solution(2)
+solution(3)
+solution(4)
+solution(5)
+solution(6)
+solution(7)
+solution(8)
+solution(9)
+solution(10)
