31ecc9507f588bed6f8d2f8bf7e2709f65aa0e8f
이종은 <dkdltmais9@gmail.com>
2021-10-22 21:13:36 +0900

    Lv1/최대공약수와 최대공배수

---
+++ 프로그래머스/Lv1/최대공약수와 최대공배수.py
@@ -0,0 +1,35 @@
+# def solution(n, m):
+#     answer = []
+#     if n > m:
+#         n,m = m,n
+#     big = 0
+#     small = 0
+#     for i in range(1,n+1):
+#         if n%i == 0 and m% i == 0:
+#             big=i
+#     small=(m*n)//big
+#     answer.append(big)
+#     answer.append(small)
+#     # print(answer)
+#     return answer
+
+def solution(n, m):
+    answer = []
+    if n > m:
+        n,m = m,n
+    big = 0
+    small =0
+    def gcd(a,b):
+        nonlocal big
+        if b%a == 0:
+            big = a
+        else:
+            gcd(b%a,a)
+    gcd(n,m)
+    small = (n*m)//big
+    answer.append(big)
+    answer.append(small)
+    # print(answer)
+    return answer
+solution(3,12)
+solution(2,5)
