805cfeb026016bf635f61ad50ec1a59c37618a40
이종은 <dkdltmais9@gmail.com>
2021-10-26 20:15:30 +0900

    스터디/20436  ZOAC 3

---
+++ 스터디/20436 ZOAC 3.py
@@ -0,0 +1,18 @@
+board = {'q':(0,0),'w':(0,1),'e':(0,2),'r':(0,3),'t':(0,4),'y':(0,5),'u':(0,6),'i':(0,7),'o':(0,8),'p':(0,9),
+         'a':(1,0),'s':(1,1),'d':(1,2),'f':(1,3),'g':(1,4),'h':(1,5),'j':(1,6),'k':(1,7),'l':(1,8),
+         'z':(2,0),'x':(2,1),'c':(2,2),'v':(2,3),'b':(2,4),'n':(2,5),'m':(2,6)}
+left,right = map(str,input().split())
+lx,ly = board[left]
+rx,ry = board[right]
+ans = list(input())
+tot = 0
+left_sol = "qwertasdfgzxcv"
+for i in ans:
+    px,py = board[i]
+    if i in left_sol:
+        tot += abs(lx - px) + abs(ly - py) + 1
+        lx,ly = px,py
+    else:
+        tot += abs(rx-px)+abs(ry-py)+1
+        rx,ry = px,py
+print(tot)
---
+++ 스터디/Main/src/Zoac.java
@@ -0,0 +1,9 @@
+package main;
+public class Zoac {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+	}
+
+}
