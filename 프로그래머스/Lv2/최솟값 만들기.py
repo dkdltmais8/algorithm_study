c26668623b23908bb508451e867e0dd0d681741d
이종은 <dkdltmais9@gmail.com>
2021-11-12 22:22:35 +0900

    Lv2/최솟값 만들기

---
+++ 프로그래머스/Lv2/최솟값 만들기.py
@@ -0,0 +1,10 @@
+def solution(A,B):
+    answer = 0
+    A.sort()
+    B.sort(reverse=True)
+    for i in range(len(A)):
+        answer+=(A[i]*B[i])
+
+    return answer
+solution([1, 4, 2],[5, 4, 4])
+solution([1,2],[3,4])
