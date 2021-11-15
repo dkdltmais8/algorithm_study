7089e923388cfa9013b94e378b7c42f923cb6fd4
이종은 <dkdltmais9@gmail.com>
2021-11-15 22:37:55 +0900

    Lv2/가장 큰 정사각형 찾기

--- 프로그래머스/Lv2/가장 큰 정사각형 찾기.py
+++ 프로그래머스/Lv2/가장 큰 정사각형 찾기.py
@@ -3,12 +3,13 @@ def solution(board):
     len_row = len(board)
     len_col = len(board[0])
     dp = [[0 for _ in range(1001)] for _ in range(1001)]
-    for i in range(1,len_row):
-        for j in range(1,len_col):
+    for i in range(1,len_row+1):
+        for j in range(1,len_col+1):
             if board[i-1][j-1] != 0:
-                dp[i][j] = min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])+1
+                dp[i][j] = min(min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1])+1
                 answer = max(answer,dp[i][j])
-                print(answer)
+                # print(answer)
+    print(answer)
     return answer**2
 solution([[0,1,1,1],[1,1,1,1],[1,1,1,1],[0,0,1,0]])
 solution([[0,0,1,1],[1,1,1,1]])
