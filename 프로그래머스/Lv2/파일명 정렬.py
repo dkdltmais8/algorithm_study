ffce95b97eb167b0c64c2e4e8efd1e81353527b4
이종은 <dkdltmais9@gmail.com>
2021-11-26 23:52:30 +0900

    프로그래머스

--- 프로그래머스/Lv2/파일명 정렬.py
+++ 프로그래머스/Lv2/파일명 정렬.py
@@ -9,12 +9,18 @@ def solution(files):
         for j in range(len(i)):
             if i[j].isdigit():
                 num+= i[j]
+                print(num)
                 isnum = 1
             elif  not isnum:
                 head += i[j]
+                print(head)
             else:
                 tail = i[j:]
+                print(tail)
                 break
         answer.append([head,num,tail])
     answer.sort(key= lambda x: (x[0].lower(),int(x[1])))
+    print(answer)
     return [''.join(ans) for ans in answer]
+solution(["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"])
+solution(["F-5 Freedom Fighter", "B-50 Superfortress", "A-10 Thunderbolt II", "F-14 Tomcat"])
