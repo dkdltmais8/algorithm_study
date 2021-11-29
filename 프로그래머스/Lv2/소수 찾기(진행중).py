660a78af94174a1c6b134828f413a69d1bc88840
이종은 <dkdltmais9@gmail.com>
2021-11-29 20:43:49 +0900

    Lv2/소수 찾기

--- 프로그래머스/Lv2/소수 찾기(진행중).py
+++ 프로그래머스/Lv2/소수 찾기(진행중).py
@@ -1,25 +1,31 @@
 from itertools import permutations
 def solution(numbers):
+    def check(n):
+        if n == 1 or n==0:
+            return False
+        for i in range(2,(n//2)+1):
+            if n%i == 0:
+                return False
+        return True
     answer = 0
-    res = list(permutations(numbers))
     numbers = list(numbers)
-    numbers.sort(reverse=True)
-    ans = ''
-    for i in numbers:
-        ans+= i
-    ans = int(ans)
-    lst = [1]*(ans+1)
-    m = int(ans**0.5)
-    for i in range(2,m+1):
-        if lst[i] == 1:
-            for j in range(i+i,ans+1,i):
-                lst[j] = 0
-    lst[0],lst[1] = 0,0
-    print(res)
-    print(lst)
-    for i,v in enumerate(lst):
-        if v == 1 and str(i) in res:
+    ans = []
+    res = []
+    for i in range(1,len(numbers)+1):
+        res.append(list(permutations(numbers,r=i)))
+    # print(res)
+    for i in res:
+        for j in i:
+            ans.append(''.join(j))
+    # print(ans)
+    for i in range(len(ans)):
+        ans[i] = int(ans[i])
+    ans = list(set(ans))
+    # print(ans)
+    for i in ans:
+        if check(int(i)):
             answer += 1
-    print(answer)
+    # print(answer)
     return answer
 solution("17")
+solution("011")
