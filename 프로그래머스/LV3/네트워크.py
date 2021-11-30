c814f66280108b72c571dec7c6eae9fb546e9aa8
이종은 <dkdltmais9@gmail.com>
2021-12-01 00:41:04 +0900

    Lv3/dfs/네트워크

--- 프로그래머스/LV3/네트워크.py
+++ 프로그래머스/LV3/네트워크.py
@@ -1,18 +1,20 @@
 def solution(n, computers):
-    def dfs(check):
-
-    answer = 0
-    ans = [[] for _ in range(n)]
-    visit = [0]*(n+1)
+    def dfs(now):
+        nonlocal answer
+        visit[now] = 1
+        for i in range(n):
+            if i != now and computers[now][i] == 1 and visit[i] == 0:
+                answer -=1
+                dfs(i)
+    answer = n
+    visit = [0]*(n)
     for i in range(n):
-        for j in range(n):
-            if i == j:
-                continue
-            if computers[i][j] == 1:
-                ans[i].append(j)
-    for i in ans:
-        if not
-    print(ans)
+        if visit[i] == 0:
+            dfs(i)
+
+
+    # print(answer)
+
     return answer
 solution(3,[[1, 1, 0], [1, 1, 0], [0, 0, 1]])
 solution(3,[[1, 1, 0], [1, 1, 1], [0, 1, 1]])
