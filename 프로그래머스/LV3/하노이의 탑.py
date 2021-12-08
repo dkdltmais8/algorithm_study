092e5a92b0534f3dc0040534aca529482b99717e
이종은 <dkdltmais9@gmail.com>
2021-12-08 23:39:35 +0900

    LV3/하노이의탑

--- 프로그래머스/LV3/하노이의 탑.py
+++ 프로그래머스/LV3/하노이의 탑.py
@@ -1,7 +1,15 @@
 def solution(n):
-    answer = [[]]
-    ans1 = [[1,3]]
-    ans2 = [ [1,2], [1,3], [2,3] ]
-    ans3 = [[1, 3], [1, 2], [3, 2], [1, 3], [2, 1], [2, 3], [1, 3]]
-    
+    def move(start,end,middle,n):
+        nonlocal answer
+        print(answer,n)
+        if n == 1:
+            return answer.append([start,end]) #start에 있는 제일 큰 것을 end로 옮긴다.
+        move(start,middle,end,n-1) #end를 거쳐 start를 middel로 옮긴다
+        print(answer, n)
+        answer.append([start,end]) #이동 기록
+        move(middle,end,start,n-1) #start를 거쳐 middle 에서 end로 옮긴다.
+    answer = []
+    move(1,3,2,n)
+    print(answer)
     return answer
+solution(3)
