49adaa2a781ee7a856effdbafd11d9a9e7a1bff4
이종은 <dkdltmais9@gmail.com>
2021-12-08 23:12:54 +0900

    하노이의탑진행

--- 프로그래머스/LV3/N-Queen.py
+++ 프로그래머스/LV3/N-Queen.py
@@ -1,17 +1,10 @@
-def solution(lines):
+def solution(n):
+    def  dfs()
     answer = 0
-    time = []
-    exe = []
-    d = len(lines)
-    for i in range(d):
-        print(lines[i])
-        # lines[i] = lines[i].split(':')
-        lines[i]=lines[i].split(' ')
-    for i in range(d):
-        minus = lines[i][2].find('s')
-        exe.append(lines[i][2][:minus])
+    board = [list(0 for i in range(n)) for _ in range(n)]
+    for i in range(n):
+        for j in range(n):
 
-    print(lines)
-    print(exe)
+    print(board)
     return answer
-solution(["2016-09-15 01:00:04.001 2.0s","2016-09-15 01:00:07.000 2s"])
+solution(4)
