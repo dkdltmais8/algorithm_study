02177b668d57eb9727fd87dbef366017648d338e
이종은 <dkdltmais9@gmail.com>
2021-12-13 21:05:09 +0900

    Lv2/다리를 지나는 트럭

--- 프로그래머스/Lv2/다리를 지나는 트럭.py
+++ 프로그래머스/Lv2/다리를 지나는 트럭.py
@@ -1,20 +1,21 @@
 from collections import deque
-
-
 def solution(bridge_length, weight, truck_weights):
     answer = 0
-    ing = deque([0] * bridge_length)
-    while 1:
-        while sum(ing) + truck_weights[0] <= weight:
-            ing.popleft()
-            ing.append(truck_weights.pop(0))
-            answer += 1
-            if not truck_weights:
-                break
-        if not truck_weights:
-            break
-        ing.popleft()
-        ing.append(0)
+    success = []
+    ing = deque([0]*bridge_length)
+    totalTruck = len(truck_weights)
+    totalWeight = 0
+    while len(success)<totalTruck:
         answer += 1
-
-    return answer + bridge_length
+        now = ing.popleft()
+        if now != 0:
+            success.append(now)
+            totalWeight-=now
+        if truck_weights:
+            if totalWeight + truck_weights[0] <= weight:
+                ing.append(truck_weights.pop(0))
+                totalWeight += ing[-1]
+            else:
+                ing.append(0)
+    return answer
+solution(2,	10,	[7,4,5,6])
