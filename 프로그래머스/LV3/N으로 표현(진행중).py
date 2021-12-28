b3e704b63e448a782e77b1ff5bbd72aa4b8fbafe
이종은 <dkdltmais9@gmail.com>
2021-12-28 20:52:47 +0900

    프로그래머스/LV3/N으로 표현

--- 프로그래머스/LV3/N으로 표현(진행중).py
+++ 프로그래머스/LV3/N으로 표현(진행중).py
@@ -1,21 +1,21 @@
 def solution(N, number):
-    answer = 0
-    ans = []
-    temp = number
-    while number>0:
-        number-=N
-        answer +=1
-        if number<0:
-            number+=N
-            answer-=1
-            answer+= 2*number
-            number = 0
-    ans.append(answer)
-    if number != 0:
-        number = temp
-        answer = 0
-    
-    print(ans,answer)
+    answer = -1
+    ans = [{}]+[{int(str(N)*i)} for i in range(1,9)]
+    for i in range(1,9):
+        for j in range(1,i):
+            for n in ans[j]:
+                for m in ans[i-j]:
+                    ans[i].add(n+m)
+                    ans[i].add(n-m)
+                    ans[i].add(n*m)
+                    if m != 0:
+                        ans[i].add(n//m)
+        if number in ans[i]:
+            answer = i
+            break
     return answer
 solution(5,12)
-solution(2,11)
+# solution(2,11)
+# 1.나눌 수 있을때까지 나누고 나머지는 1로 더해주기
+# 2.1,11,111등으로 나누고 나머지를 더해주기
+# 3.분자를 다 더해서 한번에 나눠주기
