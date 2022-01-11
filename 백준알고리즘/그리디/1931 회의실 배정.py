2710a4158c0ba9cabd61a8614984287393d74dd8
이종은 <dkdltmais9@gmail.com>
2022-01-12 01:42:49 +0900

    백준/그리디/1931 회의실 배정

--- 백준알고리즘/그리디/1931 회의실 배정.py
+++ 백준알고리즘/그리디/1931 회의실 배정.py
@@ -1,13 +1,10 @@
 n = int(input())
 arr = [list(map(int,input().split())) for _ in range(n)]
-arr.sort(key=lambda x:(x[1]))
-time = []
-cnt = 0
-for i in range(n):
-    if  arr[i][0] in time:
-        continue
-    else:
+arr.sort(key=lambda x:(x[1],x[0]))
+cnt = 1
+e = arr[0][1]
+for i in range(1,n):
+    if arr[i][0]>=e:
         cnt += 1
-        for j in range(arr[i][0],arr[i][1]):
-            time.append(j)
+        e = arr[i][1]
 print(cnt)
