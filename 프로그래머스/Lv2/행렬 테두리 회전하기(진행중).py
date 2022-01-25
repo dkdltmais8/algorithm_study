db848e839639db7907d3cc7d04dfaeddcaf88db4
이종은 <dkdltmais9@gmail.com>
2022-01-25 20:36:24 +0900

    프로그래머스 /LV2/ 행렬 테두리 회전하기

--- 프로그래머스/Lv2/행렬 테두리 회전하기(진행중).py
+++ 프로그래머스/Lv2/행렬 테두리 회전하기(진행중).py
@@ -1,21 +1,37 @@
 def solution(rows, columns, queries):
+    def rotate(lst):
+        nonlocal answer,arr
+        sr,sc = lst[0]-1,lst[1]-1
+        er,ec = lst[2]-1,lst[3]-1
+        tmp = arr[sr][sc]
+        mini = tmp
+        for r in range(sr,er):
+            move = arr[r+1][sc]
+            arr[r][sc] = move
+            mini = min(mini,move)
+        for c in range(sc,ec):
+            move = arr[er][c+1]
+            arr[er][c] = move
+            mini = min(mini,move)
+        for r in range(er,sr,-1):
+            move = arr[r-1][ec]
+            arr[r][ec] = move
+            mini = min(mini,move)
+        for c in range(ec,sc,-1):
+            move = arr[sr][c-1]
+            arr[sr][c] = move
+            mini = min(mini,move)
+        arr[sr][sc+1] = tmp
+        answer.append(mini)
     answer = []
-    arr = [list(0 for _ in range(columns)) for i in range(rows)]
-    tot = 1
+    arr =[[0]*columns for _ in range(rows)]
+    t = 1
     for r in range(rows):
         for c in range(columns):
-            arr[r][c] = tot
-            tot += 1
+            arr[r][c] = t
+            t += 1
     for i in queries:
-        tmp = []
-        start = arr[i[0]-1][i[3]-1]
-        for c in range(i[3]-1,i[1]-1,-1):
-            arr[i[0]-1][c] = arr[i[0]-1][c-1]
-            tmp.append(arr[i[0]-1][c])
-        for r in range(i[0] - 1, i[2] - 1):
-            arr[r][i[1] - 1] = arr[r + 1][i[1] - 1]
-            tmp.append(arr[r][i[1] - 1])
-        for c in range(i[1]-1)
-    print(arr)
+        rotate(i)
     return answer
-solution(6,6,[[2,2,5,4],[3,3,6,6],[5,1,6,3]])
+# solution(6,6,[[2,2,5,4],[3,3,6,6],[5,1,6,3]])
+solution(3,	3	,[[1,1,2,2],[1,2,2,3],[2,1,3,2],[2,2,3,3]])
