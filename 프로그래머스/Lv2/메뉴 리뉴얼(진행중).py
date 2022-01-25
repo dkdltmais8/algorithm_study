c5d792e04842a949a188d5c91089147bb846d852
이종은 <dkdltmais9@gmail.com>
2022-01-25 19:30:56 +0900

    프로그래머스/ LV2 / 메뉴 리뉴얼

--- 프로그래머스/Lv2/메뉴 리뉴얼(진행중).py
+++ 프로그래머스/Lv2/메뉴 리뉴얼(진행중).py
@@ -1,6 +1,14 @@
-import operator
+from itertools import combinations
+from collections import Counter
 def solution(orders, course):
     answer = []
+    for i in course:
+        can = []
+        for j in orders:
+            can += combinations(sorted(j),i)
+        cnt = Counter(can)
+        if len(cnt) != 0 and max(cnt.values())>=2:
+            answer += [''.join(k) for k in cnt if cnt[k] == max(cnt.values())]
 
-    return answer
+    return sorted(answer)
 solution(["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"],[2,3,4])
