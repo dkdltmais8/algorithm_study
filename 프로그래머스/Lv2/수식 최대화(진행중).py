fc183d7464369f2165c388da4352c98fb2f02b78
이종은 <dkdltmais9@gmail.com>
2022-01-26 19:44:27 +0900

    프로그래머스 / LV2/ 수식 최대화

--- 프로그래머스/Lv2/수식 최대화(진행중).py
+++ 프로그래머스/Lv2/수식 최대화(진행중).py
@@ -1,40 +1,39 @@
+from itertools import permutations
 def calc(a, b, op):
     a,b = int(a),int(b)
     if op == '*':
-        return a * b
+        return str(a * b)
     elif op == '+':
-        return a + b
+        return str(a + b)
     elif op == '-':
-        return a - b
+        return str(a - b)
+def cal(lst,op):
+    arr = []
+    temp = ''
+    for i in lst:
+        if i.isdigit():
+            temp += i
+        else:
+            arr.append(temp)
+            arr.append(i)
+            temp = ""
+    arr.append(temp)
+    for i in op:
+        s = []
+        while arr:
+            now = arr.pop(0)
+            if now == i:
+                s.append(calc(s.pop(),arr.pop(0),i))
+            else:
+                s.append(now)
+        arr = s
+    return abs(int(arr[0]))
+
 def solution(expression):
-    answer = 0
-    orders = [[0,1,2],[0,2,1],[1,0,2],[1,2,0],[2,1,0],[2,0,1]]
     operand = ['*','+','-']
-    num = expression
+    operand = list(permutations(operand,3))
+    ans = []
     for i in operand:
-        num = num.replace(i,' ')
-    num = num.split(' ')
-    print(num)
-    now_op = [op for op in expression if not op.isdigit()]
-    print(now_op)
-    for order in orders:
-        n = num
-        op = now_op
-        for i in range(3):
-            stack_num = []
-            stack_op = []
-            stack_num.append(n[0])
-            for j in range(len(op)):
-                stack_num.append(n[j+1])
-                stack_op.append(op[j])
-                if stack_op[-1] == operand[order[i]]:
-                    a = stack_num.pop()
-                    b = stack_num.pop()
-                    cal = stack_op.pop()
-                    stack_num.append(calc(a,b,cal))
-            n = stack_num
-            op = stack_op
-        answer = max(answer,abs(n[0]))
-        print(answer)
-    return answer
+        ans.append(cal(expression,i))
+    return ans
 solution("100-200*300-500+20")
