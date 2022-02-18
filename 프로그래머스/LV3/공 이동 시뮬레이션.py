b93d4f26f8ffb2bd411647659326b72f491d5330
이종은 <dkdltmais9@gmail.com>
2022-02-19 00:29:02 +0900

    프로그래머스/LV3/공 이동 시뮬레이션

--- 프로그래머스/LV3/공 이동 시뮬레이션.py
+++ 프로그래머스/LV3/공 이동 시뮬레이션.py
@@ -1,14 +1,34 @@
-def solution(n, m, x, y, queries):
-    sr,sc = x,y
-    er,ec = x,y
+def solution(n, m, y, x, queries):
+    sr = er = y
+    sc= ec = x
+    ans = 0
     for i,v in queries[::-1]:
         if i == 0:
             if sc != 0:
                 sc += v
-                if sc>m-1:
-                    sc = m-1
             ec += v
             if ec > m-1:
                 ec = m-1
-
-solution(2,2,0,0,[[2,1],[0,1],[1,1],[0,1],[2,1]])
+        elif i == 1:
+            sc -= v
+            if sc<0:
+                sc = 0
+            if ec !=m-1:
+                ec-=v
+        elif i == 2:
+            if sr != 0:
+                sr += v
+            er += v
+            if er >n-1:
+                er = n-1
+        elif i == 3:
+            sr -= v
+            if sr<0:
+                sr = 0
+            if er != n-1:
+                er-=v
+        if sr>n-1 or er<0 or sc>m-1 or ec<0:
+            return ans
+    return (er-sr+1) * (ec-sc+1)
+print(solution(2,2,0,0,[[2,1],[0,1],[1,1],[0,1],[2,1]]))
+print(solution(1000,1000,1,1,[[0,100001],[2,100001]]))
