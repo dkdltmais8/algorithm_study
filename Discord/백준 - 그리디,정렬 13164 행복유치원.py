9f8c5492d26384a5ac78e724323f79d5ef93b477
이종은 <dkdltmais9@gmail.com>
2022-03-09 21:18:15 +0900

    백준/1436/영화감독 숌

--- Discord/백준 - 그리디,정렬 13164 행복유치원.py
+++ Discord/백준 - 그리디,정렬 13164 행복유치원.py
@@ -9,7 +9,7 @@ else:
     for i in range(1,n):
         ans.append(arr[i]-arr[i-1])
     ans.sort()
-    for i in range(k-1):
-        ans.pop()
+    # for i in range(k-1):
+    #     ans.pop()
 
-print(sum(ans))
+print(sum(ans[0:n-k]))
---
+++ Discord/백준 - 브루트 포스 1436 영화감독 숌.py
@@ -0,0 +1,13 @@
+n = int(input())
+p = '666'
+idx = 666
+cnt = 0
+while 1:
+    if p in str(idx):
+        cnt += 1
+    if cnt == n:
+        break
+    idx += 1
+print(idx)
+
+
