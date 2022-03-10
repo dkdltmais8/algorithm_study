fb5ec5335798d3c8fa68f4f5a235b4720b39d7ec
이종은 <dkdltmais9@gmail.com>
2022-03-10 23:11:26 +0900

    백준/11048 이동하기

---
+++ Discord/백준 - DP 11048 이동하기.py
@@ -0,0 +1,17 @@
+dr = [1,-1,0,0]
+dc = [0,0,1,-1]
+n,m = map(int,input().split())
+arr = [list(map(int,input().split())) for _ in range(n)]
+dp = [[0]*m for _ in range(n)]
+dp[0][0] = arr[0][0]
+for i in range(n):
+    if i == 0:
+        for j in range(1,m):
+            dp[i][j] = dp[i][j-1]+arr[i][j]
+    for j in range(1):
+        dp[i][j] = dp[i-1][j]+arr[i][j]
+for i in range(1,n):
+    for j in range(1,m):
+        dp[i][j] = max(dp[i - 1][j] + arr[i][j], dp[i][j - 1] + arr[i][j], dp[i - 1][j - 1] + arr[i][j])
+
+print(dp[-1][-1])
