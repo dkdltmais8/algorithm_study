7084bc6169490872145ff5ee1c82a60a12c14e52
이종은 <dkdltmais9@gmail.com>
2022-03-16 12:29:07 +0900

    백준/1817/짐 챙기는 숌

---
+++ Discord/백준 - 그리디, 구현 1817 짐챙기는 숌.py
@@ -0,0 +1,17 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+n,w = map(int,input().split())
+arr = deque(list(map(int,input().split())))
+if arr:
+    cnt = 1
+    tot = 0
+    while arr:
+        now = arr.popleft()
+        if tot+now > w:
+           tot = 0
+           cnt += 1
+        tot += now
+    print(cnt)
+else:
+    print(0)
